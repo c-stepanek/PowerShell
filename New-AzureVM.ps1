a66e473bfe217c6784bb1d0796455105cc9db826
Grumpstrel <43072864+Grumpstrel@users.noreply.github.com>
2018-09-11 16:10:19 -0700

    Correcting bug with backtick use
    Running errors because of additional lines introduced by different platforms, simplifying by removing this.
    Added additional comments to the script to make things clearer.
    Suggested VM size information added also.

--- New-AzureVM.ps1
+++ New-AzureVM.ps1
@@ -1,45 +1,113 @@
+[CmdletBinding()]
 param(
-    # Name to append to the resource group
+    #Name for the resource group
     [Parameter(Mandatory=$true, Position=0)]
     [String]
-    $Name
+    $Name,
+    
+    #Virutal machine location list
+    [Parameter(Mandatory=$false, Position=1)]
+    [ValidateSet("australiaeast","australiasoutheast","brazilsouth",
+        "canadacentral","canadaeast","centralindia","centralus",
+        "eastasia","eastus","eastus2","francecentral","japaneast",
+        "japanwest","koreacentral","koreasouth","northcentralus",
+        "northeurope","southcentralus","southeastasia","southindia",
+        "uksouth","ukwest","westcentralus","westeurope","westindia",
+        "westus","westus2")]
+    [String]
+    $Location = "westus2",
+
+    #Virtual machine credentials
+    [Parameter(Mandatory=$true, Position=2)]
+    [ValidateNotNullOrEmpty()]
+    [System.Management.Automation.PSCredential]
+    $VMCred,
+
+    #Virtual machine size:
+    #Some sizes are not available in all locations
+    #Pricing can be found - https://azure.microsoft.com/en-us/pricing/details/virtual-machines/windows/
+    #Suggested max size with $150 free subscription 9/11/2018 - "Standard_D4s_v3"
+    [Parameter(Mandatory=$false, Position=3)]
+    [String]
+    $VMSize = "Standard_D1"
+
 )
 
-#Location list can be found with the 'Get-AzureRMLocation' cmdlet
-$Location = 'westus2'
-$ResourceGroup =  ($Name + "-VM-ResourceGroup")
+#AzureRM module check
+if (Get-Module -ListAvailable -Name AzureRM)
+{
+    Import-Module -Name AzureRM
+}
+else 
+{
+    Write-Warning "The AzureRM module was not found. Attempting to install the AzureRM module."
+    if (-not [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).Groups -match "S-1-5-32-544"))
+    {
+        throw "You must run this as Administrator to install the AzureRM module."
+    }
+    else
+    {
+        #We will assume you are running at least PowerShell Version 5.0
+        #and/or alread have PowerShellGet installed.
+        Install-Module -Name AzureRM -Force
+    }
+}
+
+#Datacenter location validity check
+if ( (Get-AzureRMLocation | Where {$_.Location -eq $Location}).count -eq 0 )
+{
+    throw "AzureRM location $Location not found. You can find a list of valid locations using Get-AzureRMLocation.";
+}
+
+#Machine size validity check
+if ( (Get-AzureRmVMSize -Location $Location | Where {$_.Name -match $VMSize}).count -eq 0 )
+{
+    throw "Could not find a VMSize $VMSize at Location $Location. You can find VMSizes using the Get-AzureRmVMSize -Location <location> cmdlet."
+}
+
+#$Name is appended to the ResourceGroup name to make it unique
+#$Location list can be found with the 'Get-AzureRMLocation' cmdlet
+$ResourceGroup =  ($Name + "-VM-ResourceGroup");
 
 #Create resource group
-New-AzureRmResourceGroup -ResourceGroupName $ResourceGroup -Location $Location
+Write-Verbose "Creating AzureRM Resource Group: $ResourceGroup";
+New-AzureRmResourceGroup -ResourceGroupName $ResourceGroup -Location $Location | Out-Null;
 
 #Create virtual network
-$SubnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name vSubnet -AddressPrefix 192.168.1.0/24
-$VirtualNetwork = New-AzureRmVirtualNetwork -ResourceGroupName $ResourceGroup -Location $Location -Name vNetwork -AddressPrefix 192.168.0.0/16 -Subnet $SubnetConfig
+Write-Verbose "Creating Azure Virtual Network: vSubnet";
+$SubnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name vSubnet -AddressPrefix 192.168.1.0/24;
+$VirtualNetwork = New-AzureRmVirtualNetwork -ResourceGroupName $ResourceGroup -Location $Location -Name vNetwork -AddressPrefix 192.168.0.0/16 -Subnet $SubnetConfig;
 
 #Create public IP address
-$PublicIP = New-AzureRmPublicIpAddress -ResourceGroupName $ResourceGroup -Location $Location -AllocationMethod Static -Name PublicIPAddress
+Write-Verbose "Creating a public IP address for ResourceGroup: $ResourceGroup in Location: $Location";
+$PublicIP = New-AzureRmPublicIpAddress -ResourceGroupName $ResourceGroup -Location $Location -AllocationMethod Static -Name PublicIPAddress;
 
 #Create network interface card
+Write-Verbose "Creating NIC with public IP address";
 $NIC = New-AzureRmNetworkInterface -ResourceGroupName $ResourceGroup -Location $Location -Name vNIC -SubnetId $VirtualNetwork.Subnets[0].Id -PublicIpAddressId $PublicIP.Id
 
 #Create firewall rules and network security group
-$RdpAccess = New-AzureRmNetworkSecurityRuleConfig -Name RDP -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * `
--DestinationAddressPrefix * -DestinationPortRange 3389 -Access Allow
-$HttpsAccess = New-AzureRmNetworkSecurityRuleConfig -Name HTTPS -Protocol Tcp -Direction Inbound -Priority 1010 -SourceAddressPrefix * -SourcePortRange * `
--DestinationAddressPrefix * -DestinationPortRange 443 -Access Allow
+Write-Verbose "Creating firewall rules and network security group";
+$RdpAccess = New-AzureRmNetworkSecurityRuleConfig -Name RDP -Protocol Tcp -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389 -Access Allow
+$HttpsAccess = New-AzureRmNetworkSecurityRuleConfig -Name HTTPS -Protocol Tcp -Direction Inbound -Priority 1010 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 443 -Access Allow
 $NetworkSG = New-AzureRmNetworkSecurityGroup -ResourceGroupName $ResourceGroup -Location $Location -Name NetworkSecurityGroup -SecurityRules $RdpAccess,$HttpsAccess
-Set-AzureRmVirtualNetworkSubnetConfig -Name vSubnet -VirtualNetwork $VirtualNetwork -NetworkSecurityGroup $NetworkSG -AddressPrefix 192.168.1.0/24
-Set-AzureRmVirtualNetwork -VirtualNetwork $VirtualNetwork
+Set-AzureRmVirtualNetworkSubnetConfig -Name vSubnet -VirtualNetwork $VirtualNetwork -NetworkSecurityGroup $NetworkSG -AddressPrefix 192.168.1.0/24 | Out-Null
+Set-AzureRmVirtualNetwork -VirtualNetwork $VirtualNetwork | Out-Null
 
 #Create virtual machine
-$Cred = Get-Credential
-$VM = New-AzureRmVMConfig -VMName LabVM -VMSize Standard_D1
-$VM = Set-AzureRmVMOperatingSystem -VM $VM -Windows -ComputerName LabVM -Credential $Cred -ProvisionVMAgent -EnableAutoUpdate
+Write-Verbose "Creating Virtual Machine config details";
+
+$VM = New-AzureRmVMConfig -VMName LabVM -VMSize $VMSize;
+$VM = Set-AzureRmVMOperatingSystem -VM $VM -Windows -ComputerName "LabVM-$Name" -Credential $VMCred -ProvisionVMAgent -EnableAutoUpdate
 $VM = Set-AzureRmVMSourceImage -VM $VM -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2016-Datacenter -Version latest
 $VM = Set-AzureRmVMOSDisk -VM $VM -Name OsDisk -DiskSizeInGB 128 -CreateOption FromImage -Caching ReadWrite
 $VM = Add-AzureRmVMNetworkInterface -VM $VM -Id $NIC.Id
 
-New-AzureRmVM -ResourceGroupName $ResourceGroup -Location $Location -VM $VM
+Write-Verbose "Creating virtual machine $VM.Name in ResourceGroup $ResourceGroup in Location $Location";
+New-AzureRmVM -ResourceGroupName $ResourceGroup -Location $Location -VM $VM | Out-Null
+Write-Verbose "VM creation successful!"
 
 #Show Public IP address
-Get-AzureRmPublicIpAddress -ResourceGroupName $ResourceGroup | Select-Object IpAddress
+$publicIP = (Get-AzureRmPublicIpAddress -ResourceGroupName $ResourceGroup).IpAddress;
+Write-Output "Virtual machine $VM.Name in ResourceGroup $ResourceGroup in Location $Location has been created with a Public IP $publicIP."
+Write-Output "You can use the credentials provided earlier in the process to connect to this VM with RDP."
