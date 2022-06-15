f5f19d92c9cde0f86db3b74e185bb49393ed6456
Chris Stepanek <cstepanek@live.com>
2022-06-15 14:19:42 -0700

    Update comments

--- FunWithPowerShell.psm1
+++ FunWithPowerShell.psm1
@@ -19,16 +19,19 @@ function New-PowerShellSignatureGenerator {
     }
     
     <#
-        RegEx Filter for Split
+        Output:
+        First, We split the hex string into groups of 2 using RexEx.
+        Then, the value after the comma (,) is the maximum number of substrings returned by the split operation.
+        This will be total length of the hex string divided by 2.
+        The foreach (%) takes the hex string and casts it to [int] then casts the [int] to [char]
+        Finally, we join all of the characters to form the original message.
+
+        RegEx Filter (?<=\G.{2})
         ?<= - Positive Lookbehind. Matches a group before the main expression.
         \G  - Asserts position at the end of the previous match or the start of the string for the first match.
         .   - Matches any character (except for line terminators).
         {2} - Matches the previous token exactly 2 times.
     #>
-
-    # The value after the comma (,) is the maximum number of substrings returned by the split operation.
-    # This will be total length of the hex string divided by 2.
-    # The foreach (%) takes the hex string and casts it to [int] then casts the [int] to [char]
 
     Write-Output "-join('$hexString' -split'(?<=\G.{2})',$($hexString.Length/2)|%{[char][int]`"0x`$_`"})"
 }
